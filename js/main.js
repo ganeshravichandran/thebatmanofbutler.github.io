f6232db7c8711f44f98f4430f14d21559645de99
Ganesh Ravichandran <gr2483@columbia.edu>
2016-01-13 14:12:58 -0500

    basic product designed

--- js/main.js
+++ js/main.js
@@ -1,19 +1,52 @@
-function navbarClick(sectionID, liID) {
-	var topOf = $('#' + sectionID).offset().top;
+$('#about-li').click( function() {
+	var topOf = $('body').offset().top;
 	$('body, html').animate({ scrollTop:topOf });
 
 	$('.navbar li').css('background-color', '#FB3640')	
-	$('#' + liID).css('background-color', '#1D3461')
-}
-
-$('#about-li').click( function() {
-	navbarClick('about', 'about-li')
+	$('#about-li').css('background-color', '#1D3461')
 });
 
 $('#projects-li').click( function() {
-	navbarClick('projects', 'projects-li')
+	var topOf = $('#projects').offset().top;
+	$('body, html').animate({ scrollTop:topOf });
+
+	$('.navbar li').css('background-color', '#FB3640')	
+	$('#projects-li').css('background-color', '#1D3461')
 });
 
 $('#hobbies-li').click( function() {
-	navbarClick('hobbies', 'hobbies-li')
+	var topOf = $('#hobbies').offset().top;
+	$('body, html').animate({ scrollTop:topOf });
+
+	$('.navbar li').css('background-color', '#FB3640')	
+	$('#hobbies-li').css('background-color', '#1D3461')
+});
+
+$('#arrow').click( function() {
+	$('#arrow-popup').show();
+	$('#arrow-popup').css('opacity', '1');
+});
+
+$('#kepler').click( function() {
+	$('#kepler-popup').show();
+});
+
+$('#trace').click( function() {
+	$('#trace-popup').show();
+});
+
+$('#lexicalPT').click( function() {
+	$('#lexicalPT-popup').show();
+});
+
+$('#mrMime').click( function() {
+	$('#mrMime-popup').show();
+});
+
+$('#RoboAO').click( function() {
+	$('#RoboAO-popup').show();
+});
+
+$('.close-link').click( function() {
+	$('.popup').hide();
 });
