517751f9c9e229aee50564cfe672eb2fd7090c65
Ganesh Ravichandran <gr2483@columbia.edu>
2016-01-12 19:46:50 -0500

    first commit new branch

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-var t = $("#anchor-point").offset().top;
-
-$(document).scroll(function(){
-    if($(this).scrollTop() > t)
-    {   
-        $('#voice2').css({"border-bottom":"2px solid #f4f5f8"});
-        $('#voice3').css({"border-bottom":"2px solid #2e375b"});
-    }
-});
